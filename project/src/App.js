329054800d1ab6d872246f101d0ed7be1d3335a4
Esra <esraeryilmaz7@gmail.com>
2022-12-16 15:58:09 +0300

    mui react components added

--- project/src/App.js
+++ project/src/App.js
@@ -1,6 +1,52 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
+
+
+
+function App() {
+	const [date, setDate] = React.useState('');
+
+	const handleChange = (event: SelectChangeEvent) => {
+		setDate(event.target.value);
+	};
+
+
+	return (
+		<div>
+			<FormControl sx={{ m: 5, minWidth: 180 }}>
+			<InputLabel id="demo-simple-select-autowidth-label">Date</InputLabel>
+			<Select
+				labelId="demo-simple-select-autowidth-label"
+				id="demo-simple-select-autowidth"
+				value={date}
+				onChange={handleChange}
+				autoWidth
+				label="Date"
+			>
+			<MenuItem value="">
+				<em>None</em>
+			</MenuItem>
+			<MenuItem value={1}>Son 1 saat</MenuItem>
+			<MenuItem value={2}>Son 2 saat</MenuItem>
+			<MenuItem value={3}>Son 3 saat</MenuItem>
+			</Select>
+		</FormControl>
+		</div>
+	);
+}
+
+
+
+
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +67,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
